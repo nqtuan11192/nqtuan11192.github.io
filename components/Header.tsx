89fd50784dfe27e94f09ae2a41e3d604ddcbad43
Tuan Nguyen <nqtuan0192@gmail.com>
2025-11-26 20:35:44 +0700

    Update UI/UX

--- components/Header.tsx
+++ components/Header.tsx
@@ -60,9 +60,9 @@ const Header: React.FC = () => {
             onClick={handleGiftBoxClick}
             className="text-white/90 hover:text-white transition-colors duration-300 font-medium flex items-center gap-1"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" />
-            </svg>
+            </svg> */}
             Hộp mừng cưới
           </button>
         </nav>
@@ -108,9 +108,9 @@ const Header: React.FC = () => {
             onClick={handleGiftBoxClick}
             className="text-white/90 hover:text-white transition-colors duration-300 font-medium text-lg flex items-center gap-2"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" />
-            </svg>
+            </svg> */}
             Hộp mừng cưới
           </button>
         </nav>
