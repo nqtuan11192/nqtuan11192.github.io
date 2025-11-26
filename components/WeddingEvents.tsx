6765c1f05a00e3a88feb55f085595f5b347a0f0d
Tuan Nguyen <nqtuan0192@gmail.com>
2025-11-26 13:34:01 +0700

    Minor fix

--- components/WeddingEvents.tsx
+++ components/WeddingEvents.tsx
@@ -255,7 +255,7 @@ const WeddingEvents: React.FC = () => {
                     <div className="w-24 h-0.5 bg-rose-400 mx-auto mb-4"></div>
                     <p className="text-slate-500 italic max-w-2xl mx-auto">
                         Đám cưới không phải là đích đến,<br />
-                        nó là điểm khởi đầu của một hành trình mới bên nhau.
+                        nó là điểm khởi đầu của một hành trình mới...
                     </p>
                 </div>
 
