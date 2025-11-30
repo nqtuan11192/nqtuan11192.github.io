5721c0c07d4af2897b2200890f23747dbb4e9f04
Tuan Nguyen <nqtuan0192@gmail.com>
2025-12-01 02:40:03 +0700

    Embeding all contents/stylings/fonts locally

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./App.tsx",
+        "./index.tsx",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                'heavenly': ['SVN-Heavenly', 'cursive'],
+                'heavenly-bold': ['SVN-Heavenly-Bold', 'cursive'],
+                'script': ['Great Vibes', 'cursive'],
+                'vietnamese-script': ['Pacifico', 'cursive'],
+                'serif': ['Cormorant Garamond', 'serif'],
+                'body': ['Lato', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+}
