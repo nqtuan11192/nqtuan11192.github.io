b3428e3e6c8e563d53d71bd5023ffd5c42524679
Tuan Nguyen <nqtuan0192@gmail.com>
2025-12-01 02:51:47 +0700

    Minof fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
     <footer className="bg-rose-100 text-slate-700 py-12">
       <div className="container mx-auto px-6 text-center">
         <p className="text-lg mb-2">Thank you for being part of our journey!</p>
-        <h3 className="font-heavenly text-3xl md:text-4xl text-rose-700 my-4"> Vân Anh <span className="text-rose-500">💕</span> Quốc Tuấn </h3>
+        <h3 className="font-lato-regular text-3xl md:text-4xl text-rose-700 my-4"> Vân Anh <span className="text-rose-500">💕</span> Quốc Tuấn </h3>
         <p className="text-lg text-slate-500">Made with love</p>
       </div>
     </footer>
